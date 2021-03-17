4b8a80f111e646af5d2ce10b1e83e65754c3c785
Andy Wang <v-wajian@microsoft.com>
2021-03-16 23:47:40 -0700

    3

--- Empty50/Startup.cs
+++ Empty50/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,13 +13,16 @@ namespace Empty50
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private string _mySecret = null;
         public void ConfigureServices(IServiceCollection services)
         {
+            _mySecret = Configuration["myAppSetting"];
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,12 +31,12 @@ namespace Empty50
             }
 
             app.UseRouting();
-
+            var result = string.IsNullOrEmpty(_mySecret) ? "Null" : _mySecret;
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync($"Secret is {result}");
                 });
             });
         }
